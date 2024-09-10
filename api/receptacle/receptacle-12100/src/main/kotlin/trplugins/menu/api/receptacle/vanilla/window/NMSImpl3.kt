ba245eb548f569e9f583005cd6ea062b0e60a2e4
Micalhl <m@taboo.sh>
2024-09-10 17:56:10 +0800

    [dev] 完成 1.21 NMS 实现, 等待测试

--- api/receptacle/receptacle-12100/src/main/kotlin/trplugins/menu/api/receptacle/vanilla/window/NMSImpl3.kt
+++ api/receptacle/receptacle-12100/src/main/kotlin/trplugins/menu/api/receptacle/vanilla/window/NMSImpl3.kt
@@ -1,7 +1,26 @@
 package trplugins.menu.api.receptacle.vanilla.window
 
+import net.minecraft.core.NonNullList
+import net.minecraft.network.PacketDataSerializer
+import net.minecraft.network.protocol.game.PacketPlayOutCloseWindow
+import net.minecraft.network.protocol.game.PacketPlayOutOpenWindow
+import net.minecraft.network.protocol.game.PacketPlayOutSetSlot
+import net.minecraft.network.protocol.game.PacketPlayOutWindowData
+import net.minecraft.network.protocol.game.PacketPlayOutWindowItems
+import net.minecraft.world.inventory.Containers
+import org.bukkit.Material
+import org.bukkit.craftbukkit.v1_21_R1.inventory.CraftItemStack
+import org.bukkit.craftbukkit.v1_21_R1.util.CraftChatMessage
 import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryType
+import org.bukkit.inventory.InventoryView
 import org.bukkit.inventory.ItemStack
+import taboolib.library.reflex.Reflex.Companion.getProperty
+import taboolib.library.reflex.Reflex.Companion.setProperty
+import taboolib.module.nms.dataSerializerBuilder
+import taboolib.module.nms.sendPacket
+import trplugins.menu.api.receptacle.vanilla.window.StaticInventory.inventoryView
+import trplugins.menu.api.receptacle.vanilla.window.StaticInventory.staticInventory
 
 /**
  * TrMenu
@@ -12,27 +31,99 @@ import org.bukkit.inventory.ItemStack
  */
 class NMSImpl3 : NMS() {
 
+    private val emptyItemStack: net.minecraft.world.item.ItemStack? = CraftItemStack.asNMSCopy((ItemStack(Material.AIR)))
+
+    private val windowIds = HashMap<String, Int>()
+
+    private val Player.windowId get() = windowIds[this.name] ?: 119
+
     override fun windowId(player: Player, create: Boolean): Int {
-        TODO("Not yet implemented")
+        if (createWindowId() && create) {
+            val id = player.getProperty<Int>("entity/containerCounter")!! + 1
+            player.setProperty("entity/containerCounter", id)
+            windowIds[player.name] = id
+        }
+        return player.windowId
     }
 
     override fun sendWindowsClose(player: Player, windowId: Int) {
-        TODO("Not yet implemented")
+        if (player.useStaticInventory()) {
+            StaticInventory.close(player)
+            return
+        }
+        player.sendPacket(PacketPlayOutCloseWindow.STREAM_CODEC.decode(dataSerializerBuilder {
+            writeInt(windowId)
+        }.build() as PacketDataSerializer))
     }
 
     override fun sendWindowsItems(player: Player, windowId: Int, items: Array<ItemStack?>) {
-        TODO("Not yet implemented")
+        if (player.useStaticInventory()) {
+            val inventory = player.staticInventory!!
+            items.forEachIndexed { index, item ->
+                if (index >= inventory.size) {
+                    return
+                }
+                inventory.setItem(index, item)
+            }
+            return
+        }
+        player.sendPacket(PacketPlayOutWindowItems(
+            windowId, -1,
+            NonNullList.create<net.minecraft.world.item.ItemStack>().also {
+                it.addAll(items.map(CraftItemStack::asNMSCopy))
+            },
+            emptyItemStack
+        ))
     }
 
     override fun sendWindowsOpen(player: Player, windowId: Int, type: WindowLayout, title: String) {
-        TODO("Not yet implemented")
+        if (player.useStaticInventory()) {
+            StaticInventory.open(player, type, title)
+            return
+        }
+        val windowType = Containers::class.java.getProperty<Containers<*>>(type.vanillaId, true)
+        val component = if (title.startsWith('{') && title.startsWith('}')) {
+            CraftChatMessage.fromJSON(title)
+        } else {
+            CraftChatMessage.fromString(title)[0]
+        }
+        player.sendPacket(PacketPlayOutOpenWindow(windowId, windowType, component))
     }
 
     override fun sendWindowsSetSlot(player: Player, windowId: Int, slot: Int, itemStack: ItemStack?, stateId: Int) {
-        TODO("Not yet implemented")
+        if (player.useStaticInventory()) {
+            if (windowId == -1 && slot == -1) {
+                player.itemOnCursor.type = Material.AIR
+            } else {
+                val inventory = player.staticInventory!!
+                if (slot >= 0 && slot < inventory.size) {
+                    inventory.setItem(slot, itemStack)
+                }
+            }
+            return
+        }
+        player.sendPacket(PacketPlayOutSetSlot(windowId, -1, slot, CraftItemStack.asNMSCopy(itemStack)))
     }
 
     override fun sendWindowsUpdateData(player: Player, windowId: Int, id: Int, value: Int) {
-        TODO("Not yet implemented")
+        if (player.useStaticInventory()) {
+            val inventory = player.staticInventory!!
+            val view = player.inventoryView!!
+            val property = getInventoryProperty(inventory.type, id) ?: return
+            view.setProperty(property, value)
+            return
+        }
+        player.sendPacket(PacketPlayOutWindowData.STREAM_CODEC.decode(dataSerializerBuilder {
+            writeInt(windowId)
+            writeInt(id)
+            writeInt(value)
+        }.build() as PacketDataSerializer))
+    }
+
+    /**
+     * FIXME 分包只是为了这个玩意, 否则所有 NMS 代码能写到一起的
+     */
+    private fun getInventoryProperty(type: InventoryType, id: Int): InventoryView.Property? {
+        return InventoryView.Property.values().find { (it.type == type || (it.type == InventoryType.FURNACE && type == InventoryType.BLAST_FURNACE)) && it.id == id }
     }
 }
